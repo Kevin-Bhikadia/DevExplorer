add960f752e264f168f872b0ec8d032bef8f6ede
Prit-Savaliya <savaliyaprit911@gmail.com>
2022-11-18 10:52:58 +0530

    search-feature

--- projects/views.py
+++ projects/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Project
+from .utils import searchProjects
 from .forms import ProjectForm
 
 
 def projects(request):
-    projects = Project.objects.all()
-    context = {'projects': projects}
+    projects, search_query=searchProjects(request)
+    context = {'projects': projects, 'search_query':search_query}
     return render(request, 'projects/projects.html', context)
 
 
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
+from .utils import searchProfiles
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.decorators import login_required
 
@@ -67,8 +68,8 @@ def registerUser(request):
 
 
 def profiles(request):
-    profiles = Profile.objects.all()
-    context = {'profiles': profiles}
+    profiles, search_query = searchProfiles(request) 
+    context = {'profiles': profiles, 'search_query':search_query}
     return render(request, 'users/profiles.html', context)
 
 
